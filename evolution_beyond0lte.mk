eb79572f79748b0aae75128452963151dfe4c41e
no-leaks-inside <florianvictorcristea@gmail.com>
2022-05-29 00:36:26 +0300

    beyond0lte: Adapt For Evolution X snow

--- evolution_beyond0lte.mk
+++ evolution_beyond0lte.mk
@@ -25,18 +25,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/samsung/beyond0lte/device.mk)
 
 ## Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+## Inherit some common Evolution X stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_beyond0lte
+PRODUCT_NAME := evolution_beyond0lte
 PRODUCT_DEVICE := beyond0lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G970F
